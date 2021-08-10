f0093a0e3a2fe502706b33af3630f1818a0d85b5
Deric Walintukan <deric@loopapps.com>
2021-08-10 00:18:10 -0700

    cleanup

--- index.js
+++ index.js
@@ -1,64 +1,80 @@
-var WebSocketClient = require('websocket').client;
+var WebSocketClient = require('websocket').client
 const { v4: uuidv4 } = require('uuid')
 
-const wsEndpoint = 'ws://localhost:4000/socket/websocket'
-const liveStreamId = 'GeEk8y'
+// Config
+const WS_ENDPOINT = 'ws://localhost:4000/socket/websocket'
+const LIVE_STREAM_ID = 'GeEk8y'
+const MAX_CONNECTIONS = 1000
 
+// State
 const results = {}
-const users = 1000
 let curr = 0
-let success = 0
-let failure = 0
+let successCount = 0
+let failureCount = 0
+
+const onConnectFailed = (error) => {
+  failureCount++
+}
+
+const onError = (error) => {
+  console.log("Connection Error: " + error.toString())
+}
+
+const onClose = () => {
+  // console.log('Connection Closed')
+}
+
+const onConnect = (connection) => {
+  successCount++
+  
+  connection.on('error', (error) => onError(error))
+  connection.on('close', () => onClose())
+  connection.on('message', (message) => onMessage(message))
+
+  connection.send(JSON.stringify({
+    topic: `live_stream:${LIVE_STREAM_ID}`,
+    ref: 1, 
+    payload: {"username": ""},
+    event: "phx_join"
+  }))
+}
 
 const report = () => {
-  if (curr === users) {
+  if (curr === MAX_CONNECTIONS) {
     console.log('===== RESULTS =====')
-    console.log(`SUCCESS: ${success}`)
-    console.log(`FAILURE: ${failure}`)
+    console.log(`SUCCESS: ${successCount}`)
+    console.log(`FAILURE: ${failureCount}`)
     console.log(results)
     console.log('===================')
   }
 }
 
-const constructUrl = () => `${wsEndpoint}?guest_id=${uuidv4()}`
+const onMessage = (message) => {
+  if (message.type === 'utf8') {
+    const json = JSON.parse(message.utf8Data)
+    if (json.event === "join_batch") {
+      const count = json.payload.recent_join
+      results[json.payload.id] = count
+      report()
+    }
+  }
+}
+
+const constructUrl = () => {
+  const url = new URL(WS_ENDPOINT)
+  url.search = `?guest_id=${uuidv4()}`
+  return url
+}
 
 const connectToLiveStreamChannel = () => {
   const client = new WebSocketClient()
-  client.on('connect', (connection) => {
-      success++
-
-      connection.on('error', (error) => {
-        console.log("Connection Error: " + error.toString())
-      })
-      connection.on('close', function() {
-        // console.log('Connection Closed')
-      })
-      connection.on('message', (message) => {
-        if (message.type === 'utf8') {
-          const json = JSON.parse(message.utf8Data)
-          if (json.event === "join_batch") {
-            const count = json.payload.recent_join
-            results[json.payload.id] = count
-            report()
-          }
-        }
-      })
-  
-      connection.send(JSON.stringify({
-        topic: `live_stream:${liveStreamId}`,
-        ref: 1, 
-        payload: {"username": ""},
-        event: "phx_join"
-      }))
-  })
-  client.on('connectFailed', (error) => {
-    failure++
-  })
-  
+  client.on('connect', (connection) => onConnect(connection))
+  client.on('connectFailed', (error) => onConnectFailed(error))
   client.connect(constructUrl());
 }
 
-for (i = 0; i < users; i++) {
+// Run
+for (i = 0; i < MAX_CONNECTIONS; i++) {
   connectToLiveStreamChannel()
   curr++
 }
